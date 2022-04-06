217f8ae9f8e613ea5d41980a8227e24977dde6fe
niloysh <niloysaha.ns@gmail.com>
2022-04-06 12:57:28 -0400

    Update nav js

--- themes/niloysh/static/js/burger.js
+++ themes/niloysh/static/js/burger.js
@@ -1,13 +1,16 @@
 const menuBtn = document.querySelector('.menu-btn');
+const sideBar = document.querySelector('.sidebar-content')
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
     if(!menuOpen){
         menuBtn.classList.add('open');
         menuOpen = true;
+        sideBar.setAttribute('data-visible', true);
     }
     else {
         menuBtn.classList.remove('open');
         menuOpen = false;
+        sideBar.setAttribute('data-visible', false);
     }
 })
