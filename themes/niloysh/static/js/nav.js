6db99da31b455c232fdbe612dddac87c0574bc54
niloysh <niloysaha.ns@gmail.com>
2022-04-13 00:32:10 -0400

    Fix active links on hosted domain

--- themes/niloysh/static/js/nav.js
+++ themes/niloysh/static/js/nav.js
@@ -20,7 +20,7 @@ menuBtn.addEventListener('click', () => {
 /* highlight current link in navbar */
 const navItem = document.querySelectorAll('.navbar a');
 navItem.forEach(el => {
-    if (el.getAttribute('href') === (location.href)) {
+    if (el.pathname === (location.pathname)) {
       el.classList.add("active")
     }
   })
